1ccd5b70747fd7e7689cb0257ba315787acfb912
Bryce <bryce@brycedev.com>
2019-01-05 01:07:28 -0500

    -

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 require('setimmediate')
 const isDev = window.location.host !== "huddle.group"
-const dbPrefix = 'aegaaeg[o389807tex'
+const dbPrefix = 'aegaaeg389807tex'
 
 import Vue from 'vue'
 import App from './App.vue'
--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -15,7 +15,7 @@
         <div class="w-full flex-grow flex-col -mt-20 md:mr-4 z-50">
           <div class="w-full self-center text-black text-center py-2 px-4 cursor-pointer flex justify-center items-center mb-4 h-12">
             <div class="sm:flex-grow hidden sm:flex">
-              <div class="flex">
+              <!-- <div class="flex">
                 <div class="flex -mb-px mr-6">
                   <router-link to="/discover" class="no-underline flex items-center text-white uppercase hover:opacity-100 opacity-75" active-class="opacity-100">
                    <img src="../assets/hourglass.svg" alt="" width="12"><span class="ml-2 text-sm cursor-pointer">Newest</span>
@@ -26,7 +26,7 @@
                     <img src="../assets/fire.svg" alt="" width="12"><span class="ml-2 text-sm cursor-pointer">Active</span>
                   </router-link>
                 </div>
-              </div>
+              </div> -->
             </div>
           </div>
           <div class="flex flex-wrap">        
