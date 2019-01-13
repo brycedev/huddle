70f73669919e1c145bd0d72831511602e225dd50
Bryce <bryce@brycedev.com>
2019-01-13 00:32:01 -0500

    -

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 require('setimmediate')
 const isDev = process.env.NODE_ENV == 'development'
-const dbPrefix = 'test'
+const dbPrefix = 'test123'
 
 import Vue from 'vue'
 import App from './App.vue'
